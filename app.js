4a7a63a76cef361e9b908166152f00508bf9fd4e
Emmanuel Tweve <emmanueltweve09@gmail.com>
2023-02-19 19:21:50 +0300

    Complete ejs socket setup

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 const express = require("express");
+const path = require("path");
+const mysql = require("mysql2");
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const port = new SerialPort('COM2');
+const parser = new Readline();
+port.pipe(parser);
+
+
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Emma2030",
+  database: "patient"
+})
+
+db.connect((err)=>{
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log("Connected to database succefully..");
+});
 
 const http = require("http");
 const server = http.createServer(app);
@@ -7,19 +33,37 @@ const {Server} = require("socket.io");
 const io = new Server(server);
 
 app.get("/", (req,res)=>{
-  res.send("Hello world!");
-})
-
-app.get("/home", (req,res)=>{
+  db.query("SELECT * FROM health_data", (error, data)=>{
+    if(error){
+      console.log(error);
+    }else{
+      console.log(data);
+    }
+  })
   res.sendFile(__dirname+"/index.html");
 })
 
 app.get("/dashboard", (req, res)=>{
-  res.sendFile(__dirname+"/dashboard.html");
+  res.render("dashboard", {name: "Emmanuel"});
 })
 
 io.on("connection", (socket)=>{
   console.log("User is connected");
+  socket.on("disconnect", ()=>{
+    console.log("User disconnected");
+  })
+});
+
+parser.on('data', (data) => {
+  console.log(data);
+  console.log(typeof(data));
+  io.emit('data', data);
+  const value = Number(data);
+  db.execute('INSERT INTO health_data (heart_rate) VALUES (?)', [value]);
+});
+
+port.on('error', (err) => {
+  console.log(err.message);
 });
 
 server.listen(3000, ()=>{
